c32ca52f8762818e3bd1db56dcc2eaa3d56ae2bb
AbdulAhad-150 <abdulahad150@gmail.com>
2023-09-30 10:35:27 -0700

    variable for number

---
+++ app.js
@@ -0,0 +1,17 @@
+// Question 1
+var age ="I am 20 year old"
+alert(age);
+
+// Question 2
+var visitor ="You have visited this site 14 times"
+alert(visitor);
+
+// Question 3
+var birthyear ="My birth year is 2003"
+document.write(birthyear)
+
+// Question 4
+var ordername ="Ali"
+var product_title ="T-shirt"
+var quantity ="5"
+document.write(ordername)
